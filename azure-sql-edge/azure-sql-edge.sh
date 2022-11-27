77d4564ddaecac1ec4ca5cf0db84a24957fb9d0e
neilmarshall <neil.marshall@dunelm.org.uk>
2022-11-27 22:26:13 +0000

    add Azure SQL Edge script

---
+++ azure-sql-edge/azure-sql-edge.sh
@@ -0,0 +1,12 @@
+sudo docker run
+docker container run \
+    --detach \
+    --cap-add SYS_PTRACE \
+    --env ACCEPT_EULA=1 \
+    --env MSSQL_SA_PASSWORD=$SA_PASSWORD \
+    --memory=2g \
+    --name azuresqledge \
+    --publish 1433:1433 \
+    --restart unless-stopped \
+    --volume azuresqledge_mssqldata:/var/opt/mssql \
+    mcr.microsoft.com/azure-sql-edge
