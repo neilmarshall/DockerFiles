8ea072490e16085911e54f5270f045ca28efc068
neilmarshall <neil.marshall@dunelm.org.uk>
2022-11-27 22:25:50 +0000

    add Seq script

---
+++ seq/seq.sh
@@ -0,0 +1,12 @@
+PH=$(echo 'password' | docker run --rm -i datalust/seq config hash)
+
+docker run \
+    --name seq \
+    -d \
+    --restart unless-stopped \
+    -e ACCEPT_EULA=Y \
+    -e SEQ_FIRSTRUN_ADMINPASSWORDHASH="$PH" \
+    -v seq:/data \
+    -p 8001:80 \
+    -p 5341:5341 \
+    datalust/seq
